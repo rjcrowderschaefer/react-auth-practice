14986571807a9a508391fda4b2b819cfafc9a41f
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-07-11 19:45:47 -0400

    Set user auth with sessionStorage

--- react-auth-practice/src/App.js
+++ react-auth-practice/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import {Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Preference from './components/Preference';
+import Login from './components/Login';
+import useToken from './components/useToken';
 
 function App() {
+
+  const { token, setToken } = useToken();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <>
       <div className="wrapper">
